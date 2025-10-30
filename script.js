15362cfbd52f6293147d972f5293bc82f4522880
Roseline <rdami007@fiu.edu>
2025-10-30 15:17:15 -0400

    notes for the javascript

--- script.js
+++ script.js
@@ -82,11 +82,16 @@ document.addEventListener('DOMContentLoaded', () => {
     requestNotificationPermission() //ask the user if we can send notifications 
 })
 
+//Step 2 - Get the forecast using the location the user entered
 const getForecast = (location) => {
+    //Get the forecast from the list
     let locationKey = location.toLowerCase()
     let selectedForecast = mockForecast[locationKey]
+
+    //Save this forecast
     forecastData = selectedForecast
 
+    //Set up the notification
     let notificationText = ""
 
     //to not spam users with notifs, we'll loop through the forecast data we retrieved
@@ -98,21 +103,30 @@ const getForecast = (location) => {
         }
     });
 
+    // Send the notification
     console.log('notification text:', notificationText)
     if(notificationText) { // if the list of sunny days isn't empty, we'll send a notif
         createNotification(notificationText)
     }
 
+    // Return the forecast data for display on the page!
     return selectedForecast
 }
 
+// Step 3 - Display the forecast data on the page using the DOM
 const displayMockForecast = (forecast, index) => {
     console.log(forecast)
+    //Get all the DOM objects we're going to edit
     const date = document.getElementById('date-detail')
     const weather = document.getElementById('weather-detail')
     const temp = document.getElementById('temp-detail')
     const humidity = document.getElementById('humidity-detail')
     const forecastImage = document.getElementById('forecast-img')
+
+    // Change their inner text attributes
+    // Option chaining - null safety, if there's no currForecast it prevents the script from crashing by just returning undefined
+    // Nullish Coalescing Operator (??) - if the currForecast is null, then use the default value. In this case, a dash (-)
+        // Both of these allow us to prevent the script from crashing if the forecast data is empty and display some default values instead
 
     console.log(index)
     const currForecast = forecast[index]
@@ -120,17 +134,25 @@ const displayMockForecast = (forecast, index) => {
     weather.innerText = currForecast?.weather ?? "-"
     temp.innerText = currForecast?.temp ?? "-"
     humidity.innerText = currForecast?.humidity ?? "-"
-    forecastImage.src = `${currForecast?.weather.toLowerCase() ?? "sunny"}.jpg`
+    forecastImage.src = `${currForecast?.weather.toLowerCase() ?? "sunny"}.jpg` //Template literal strings - we can put variables in them
 }
 
+// Step 1 - write the submissions handler
 const handleSubmit = () => {
+
+    //get the location the user requested
     const locationInput = document.getElementById("location-input")
     const location = locationInput.value
     
+    //get the forecast
     const forecast = getForecast(location)
+
+    //display the forecast data
+    //We'll display the forecast data at index 1 to start with, since that's day 0
     displayMockForecast(forecast, 1)
 }
 
+// Step 5 - Other handlers for the next and back buttons
 // just moves through the forecast array
 const handleNext = () => {
     day += 1
@@ -143,8 +165,10 @@ const handlePrev = () => {
 }
 
 //-----------------------------------------------------------------
+// Step 6
 //notifications
 
+// First:
 //request permission to send notifications
 function requestNotificationPermission() {
     if (!("Notification" in window)) { // it's actually a bit hard to check whether a browser supports notifs
@@ -159,6 +183,7 @@ function requestNotificationPermission() {
     });
 }
 
+// Then:
 // create a notif
 function createNotification(text) {
     if(Notification?.permission === "granted") {
